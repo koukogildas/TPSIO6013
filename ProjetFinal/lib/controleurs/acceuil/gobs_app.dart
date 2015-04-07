d615de5937f6f73e0a08639727d27595ceb089b3
unknown <u6195@D619560V.ADPROD.RAMQ.GOV>
2015-04-07 17:12:49 -0400

    acceuil

---
+++ ProjetFinal/lib/controleurs/acceuil/gobs_app.dart
@@ -0,0 +1,9 @@
+//import 'package:paper_elements/paper_input.dart';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('gobs-app')
+class GobsApp extends PolymerElement {
+  
+  GobsApp.created() : super.created();
+  
+}
